cda38a7c2ea000bbbd8dd6d2f81b70e8f0709b64
Usama Bhakrani <usamabhakrani@gmail.com>
2024-07-31 22:16:21 +0500

    right sidebar finalized with sections

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,11 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+import RenderTag from "./RenderTag";
+
 const RightSideBar = () => {
+  const hotQuestions = [
+    { _id: 1, title: "How do i use an express server?" },
+    { _id: 2, title: "How do i use an express server?" },
+    { _id: 3, title: "How do i use an express server?" },
+    { _id: 4, title: "How do i use an express server?" },
+  ];
+
+  const popularTags = [
+    { _id: 1, name: "Javascript", totalQuestions: 100 },
+    { _id: 1, name: "Next js", totalQuestions: 70 },
+    { _id: 1, name: "Redux", totalQuestions: 30 },
+    { _id: 1, name: "Html", totalQuestions: 50 },
+  ];
+
   return (
     <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[350px] custom-scrollbar">
       <div className="">
         <h3 className="h3-bold text-dark-200_light900">Recent Invoices</h3>
-        <div className="mt-7 flex w-full flex-col gap-[30px]"></div>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map((question) => {
+            return (
+              <Link
+                key={question._id}
+                href={`questions/${question._id}`}
+                className="flex cursor-pointer items-center justify-between gap-7"
+              >
+                <p className="body-medium text-dark500_light700">
+                  {question.title}
+                </p>
+                <Image
+                  src="/assets/icons/chevron-right.svg"
+                  alt="chevron Right"
+                  height={20}
+                  width={20}
+                  className="invert-colors"
+                />
+              </Link>
+            );
+          })}
+        </div>
       </div>
-      <div className="mt-16">test 2</div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark-200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => {
+            return (
+              <RenderTag
+                _id={tag._id}
+                key={tag._id}
+                name={tag.name}
+                totalQuestions={tag.totalQuestions}
+                showCount
+              />
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
